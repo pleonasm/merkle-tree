e07614ac203a2f282b2d7a55e6b9208b5d045a51
Matt Nagi <mattnagi@quickenloans.com>
2013-05-02 22:14:33 -0400

    Added node duplicator

---
+++ src/Pleo/Merkle/OneChildDuplicateNode.php
@@ -0,0 +1,41 @@
+<?php
+namespace Pleo\Merkle;
+
+/**
+ * Acts like a TwoChildrenNode but accepts one data item and duplicates it
+ */
+class OneChildDuplicateNode implements ITreeNode
+{
+    /**
+     * @var TwoChildrenNode
+     */
+    private $node;
+
+    /**
+     * @param TwoChildrenNode $node
+     */
+    public function __construct(TwoChildrenNode $node)
+    {
+        $this->node = $node;
+    }
+
+    /**
+     * @throws UnexpectedValueException
+     * @return string|null
+     */
+    public function hash()
+    {
+        return $this->node->hash();
+    }
+
+    /**
+     * @param string|ITreeNode $data
+     * @throws LogicException
+     * @throws InvalidArgumentException
+     * @return null
+     */
+    public function data($data)
+    {
+        return $this->node->data($data, $data);
+    }
+}
